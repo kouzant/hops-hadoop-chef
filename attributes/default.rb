afab5d0205f37f251984d893ee686542edcd3d91
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2018-03-21 16:00:14 +0100

    [test_HOPS-187]  test

--- attributes/default.rb
+++ attributes/default.rb
@@ -197,7 +197,7 @@ default['hops']['log_level']                = "DEBUG"
 
 default['hops']['hdfs']['blocksize']        = "134217728"
 
-default['dal']['download_url']              = "#{node['download_url']}/ndb-dal-#{node['hops']['version']}-#{node['ndb']['version']}.jar"
+default['dal']['download_url']              = "#{node['download_url']}/antonis/ndb-dal-#{node['hops']['version']}-#{node['ndb']['version']}.jar"
 default['dal']['lib_url']                   = "#{node['download_url']}/libhopsyarn-#{node['hops']['version']}-#{node['ndb']['version']}.so"
 default['nvidia']['download_url']           = "#{node['download_url']}/nvidia-management-#{node['hops']['version']}-#{node['ndb']['version']}.jar"
 default['hops']['libnvml_url']              = "#{node['download_url']}/libhopsnvml-#{node['hops']['version']}.so"
